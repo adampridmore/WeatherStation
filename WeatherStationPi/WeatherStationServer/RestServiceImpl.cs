80d405aa7074ce56054c91b817ab47384df7b3bc
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-09 20:55:40 +0000

    - Removed .svc endpoint, and Service1.cs

---
+++ WeatherStationPi/WeatherStationServer/RestServiceImpl.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherStationServer
+{
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        public string XmlData(string id)
+        {
+            return "Request id: " + id;
+        }
+
+        public string JsonData(string id)
+        {
+            return "Request id: " + id;
+        }   
+    }
+}
