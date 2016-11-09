bd70461d0666285b56a858017fef318c9f7913a4
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-09 20:33:37 +0000

    - Initial commit.

---
+++ WeatherStationPi/WeatherStationServer/IRestServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WeatherStationServer
+{
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+             ResponseFormat = WebMessageFormat.Xml,
+             BodyStyle = WebMessageBodyStyle.Wrapped,
+             UriTemplate = "xml/{id}")]
+        string XmlData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+             ResponseFormat = WebMessageFormat.Json,
+             BodyStyle = WebMessageBodyStyle.Wrapped,
+             UriTemplate = "json/{id}")]
+        string JsonData(string id);
+    }
+}
