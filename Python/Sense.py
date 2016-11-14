9e7ff0c9034b5cf9ad49397d514b45a59221c072
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-14 14:28:59 +0000

    - Changed the python app to loop forever.  - Tidied up the posting of data.  - Added some project notes.txt

--- Python/Sense.py
+++ Python/Sense.py
@@ -1,45 +1,32 @@
 from sense_hat import SenseHat
 import datetime 
 import urllib2,json
-
+import time
 
 sense = SenseHat()
-
 # sense.show_message("Hello world!")
 
-current_time = datetime.datetime.now().time() 
-
-temp = sense.get_temperature() 
-
-print(temp)
-
-# url = 'http://www.google.com/someservice'
-# url = 'http://calf/WeatherStationServer/api/serverDateTimeUtc'
-# url = 'http://calf/WeatherStationServer/api/test'
-url = 'http://calf/WeatherStationServer/api/dataPoints'
-
-postdata = { 
-	"DataPoints" : [{
-	    "SensorValueText" : str(temp)
-    }]
-}
-
-# POST: http://localhost:59653/api/dataPoints
-# {
-#	"DataPoints" : [{
-#		"SensorValueText" : "ValueText"
-#		}]
-#	}/
-
-req = urllib2.Request(url, postdata)
-req.add_header('Content-Type','application/json')
-data = json.dumps(postdata)
-
-#response = urllib2.urlopen(req,data)
-response = urllib2.urlopen(req, data)
-print(response.read())
 
 
+while True:
+	temp = sense.get_temperature() 
 
+	postdata = { 
+		"DataPoints" : [{
+			"SensorValueText" : str(temp)
+		}]
+	}
 
+	# url = 'http://www.google.com/someservice'
+	# url = 'http://calf/WeatherStationServer/api/serverDateTimeUtc'
+	# url = 'http://calf/WeatherStationServer/api/test'
+	url = 'http://calf/WeatherStationServer/api/dataPoints'
 
+	req = urllib2.Request(url, postdata)
+	req.add_header('Content-Type','application/json')
+
+	response = urllib2.urlopen(req, json.dumps(postdata))
+
+	
+	print(str(datetime.datetime.now().time()) + ", " + str(temp) + ", " + response.read())
+	time.sleep(5)
