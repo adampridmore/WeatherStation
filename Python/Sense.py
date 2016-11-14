336866456a927bc0fb384a3c41070580576c59f4
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-14 15:02:32 +0000

    - Scratch code for posting more data from client.

--- Python/Sense.py
+++ Python/Sense.py
@@ -1,32 +1,33 @@
-from sense_hat import SenseHat
+# from sense_hat import SenseHat
 import datetime 
-import urllib2,json
+import urllib.request
+import json
 import time
 
-sense = SenseHat()
+#sense = SenseHat()
 # sense.show_message("Hello world!")
 
-
-
 while True:
-	temp = sense.get_temperature() 
-
-	postdata = { 
-		"DataPoints" : [{
-			"SensorValueText" : str(temp)
-		}]
-	}
-
-	# url = 'http://www.google.com/someservice'
-	# url = 'http://calf/WeatherStationServer/api/serverDateTimeUtc'
-	# url = 'http://calf/WeatherStationServer/api/test'
-	url = 'http://calf/WeatherStationServer/api/dataPoints'
-
-	req = urllib2.Request(url, postdata)
-	req.add_header('Content-Type','application/json')
-
-	response = urllib2.urlopen(req, json.dumps(postdata))
-
+        #temp = sense.get_temperature()
+        temp = 100
+        postdata = {
+                "DataPoints" : [{
+                        "SensorValueText" : str(temp)
+                }]
+        }
+        # url = 'http://www.google.com/someservice'
+        # url = 'http://calf/WeatherStationServer/api/serverDateTimeUtc'
+        # url = 'http://calf/WeatherStationServer/api/test'
+        # url = 'http://calf/WeatherStationServer/api/dataPoints'
+        #url = 'http://localhost/WeatherStationServer/api/dataPoints'
+        url = 'http://localhost:59653/api/dataPoints'
+       
+        req = urllib.request.Request(url)
+        req.add_header('Content-Type','application/json')
+               
+        jsondata = json.dumps(postdata)
+        jsondatabytes = jsondata.encode('utf-8')
+        response = urllib.request.urlopen(req, jsondatabytes)
 	
-	print(str(datetime.datetime.now().time()) + ", " + str(temp) + ", " + response.read())
-	time.sleep(5)
+        print(str(datetime.datetime.now().time()) + ", " + str(temp) + ", " + str(response.read()))
+        time.sleep(5)
