8999f8505d0842dc3937174da4009b368464142e
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-16 20:24:26 +0000

    - Now shows three charts for temp, humidity and pressure.

--- WeatherStationPi/WeatherAnalytics/Script.fsx
+++ WeatherStationPi/WeatherAnalytics/Script.fsx
@@ -1,9 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 #r @"..\Repository\bin\Debug\Repository.dll"
+//#r @"..\packages\FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+#r @"..\packages\FSharp.Charting.0.90.14\lib\net40\FSharp.Charting.dll"
 
 open Repository
 open Repository.RepositoryDto
+open FSharp.Data
+open FSharp.Charting
 
 // Define your library scripting code here
 
@@ -12,7 +16,15 @@ let repository = new Repository.DataPointRepository()
 let dataPointToString (dp:DataPoint) = 
     sprintf "%s - %s" dp.SensorValueText (dp.TimeStamp.Value.ToString("O"))
 
-repository.GetDataPoints("weatherStation1_raspberrypi", "Temperature")
-|> Seq.map dataPointToString
-|> Seq.iter (printfn "%A")
+let chartForStationSendor stationId sensorType =
+    repository.GetDataPoints(stationId, sensorType)
+    //|> Seq.map dataPointToString
+    //|> Seq.iter (printfn "%A")
+    |> Seq.map (fun (dp:DataPoint) -> dp.TimeStamp.Value, dp.SensorValueNumber)
+    |> Seq.filter (fun (_ , v) -> v <> 0.0)
+    |> Chart.Line |> Chart.WithTitle sensorType
 
+[   chartForStationSendor "weatherStation1_raspberrypi" "Temperature";
+    chartForStationSendor "weatherStation1_raspberrypi" "Humidity";
+    chartForStationSendor "weatherStation1_raspberrypi" "Pressure"]
+|> Chart.Show
