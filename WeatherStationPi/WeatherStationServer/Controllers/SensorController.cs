20148eb68d1ed64250bd5f44cbdb9ba0ab6037a7
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-21 20:05:53 +0000

    - Now a link from station page to sensor guage.  - Different sensor type guages have different settings / options.

--- WeatherStationPi/WeatherStationServer/Controllers/SensorController.cs
+++ WeatherStationPi/WeatherStationServer/Controllers/SensorController.cs
@@ -49,26 +49,10 @@ namespace WeatherStationServer.Controllers
 
             var data = new List<Tuple<string, decimal>>
             {
-                new Tuple<string, decimal>("C", FotmatSensorNumber(dataPoint))
+                new Tuple<string, decimal>(GetUnit(dataPoint.SensorType), FotmatSensorNumber(dataPoint))
             };
 
-            var max = 30;
-            var min = -10;
-            var options = new Configuration.Options
-            {
-                yellowFrom = min,
-                yellowTo = 0,
-                yellowColor = "#aaaaff",
-                //greenFrom = 15,
-                //greenTo = 25,
-                redFrom = 30,
-                redTo = max,
-                max = max,
-                min = min,
-                minorTicks = 5,
-                is3D = true,
-                displayAnnotations = true,
-            };
+            var options = CreateOptions(dataPoint.SensorType);
 
             var guage = Chart.Gauge(
                 data,
@@ -80,9 +64,57 @@ namespace WeatherStationServer.Controllers
             return guage.GetInlineHtml();
         }
 
+        private static string GetUnit(string sensorType)
+        {
+            switch (sensorType)
+            {
+                case "Temperature":
+                    return "C";
+                case "Pressure":
+                    return "mb";
+                case "Humidity":
+                    return "RH";
+                default:
+                    return "";
+            }
+        }
+
+        private static Configuration.Options CreateOptions(string sensorType)
+        {
+            switch (sensorType)
+            {
+                case "Temperature":
+                    var max = 30;
+                    var min = -10;
+                    return new Configuration.Options
+                    {
+                        yellowFrom = min,
+                        yellowTo = 0,
+                        yellowColor = "#aaaaff",
+                        //greenFrom = 15,
+                        //greenTo = 25,
+                        redFrom = 30,
+                        redTo = max,
+                        min = min,
+                        max = max,
+                        minorTicks = 5,
+                    };
+                case "Pressure":
+                    return new Configuration.Options
+                    {
+                        min = 900,
+                        max = 1100,
+                        greenFrom = 970,
+                        greenTo = 980,
+                    };
+                default:
+                    return new Configuration.Options();
+            }
+        }
+
         private static decimal FotmatSensorNumber(DataPoint dataPoint)
         {
-            return Math.Round((decimal)dataPoint.SensorValueNumber, 1);
+            return Math.Round((decimal) dataPoint.SensorValueNumber, 1);
         }
     }
 }
