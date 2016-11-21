3f86b40b4b9cf4734b229ae46e3b207ec88cf78b
Adam Pridmore <adam_pridmore@hotmail.com>
2016-11-21 15:32:04 +0000

    - Added V1 -> V2 migration.

---
+++ WeatherStationPi/Repository/Migrations/201611211524301_V2.cs
@@ -0,0 +1,28 @@
+namespace Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class V2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.DataPoints");
+            AlterColumn("dbo.DataPoints", "StationId", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.DataPoints", "SensorType", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.DataPoints", new[] { "StationId", "SensorType", "SensorTimestampUtc" });
+            DropColumn("dbo.DataPoints", "Id");
+            DropColumn("dbo.DataPoints", "SensorValueText");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.DataPoints", "SensorValueText", c => c.String());
+            AddColumn("dbo.DataPoints", "Id", c => c.Int(nullable: false, identity: true));
+            DropPrimaryKey("dbo.DataPoints");
+            AlterColumn("dbo.DataPoints", "SensorType", c => c.String());
+            AlterColumn("dbo.DataPoints", "StationId", c => c.String());
+            AddPrimaryKey("dbo.DataPoints", "Id");
+        }
+    }
+}
