4cecd52b6765a814ccc985ebd52626c962c0000f
Adam Pridmore <adam_pridmore@hotmail.com>
2017-01-31 20:07:47 +0000

    - Added a basic angular app.

---
+++ WeatherStationPi/WeatherStationServer/Content/Angular/app.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+// Declare app level module which depends on views, and components
+//angular.module('myApp', [
+//  'ngRoute',
+//  'myApp.view1',
+//  'myApp.view2',
+//  'myApp.version'
+//]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+//    //$locationProvider.hashPrefix('!');
+//    $loationnProvider.html5Mode(true);
+
+//    $routeProvider.otherwise({ redirectTo: '/view1' });
+//}]);
+
+angular
+    .module('myApp', [])
+    .controller('myController', ['$scope', function($scope) {
+        $scope.message = 'Hello World from angular!';
+    }]);
