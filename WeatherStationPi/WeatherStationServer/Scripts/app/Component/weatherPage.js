704b029ba05a7a7a2079863b05ae8197c886e58e
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-02 22:10:54 +0000

    - Added basic page routing.

---
+++ WeatherStationPi/WeatherStationServer/Scripts/app/Component/weatherPage.js
@@ -0,0 +1,13 @@
+﻿angular.module("myApp")
+    .component("weatherPage",
+    {
+        bindings: {
+            name: "@"
+        },
+        controller: [
+            function () {
+            }
+        ],
+        templateUrl: "Scripts/app/Component/weatherPage-template.html"
+        //template: "Fish: {{$ctrl.name}}"
+    });
--- WeatherStationPi/WeatherStationServer/Scripts/app/app.js
+++ WeatherStationPi/WeatherStationServer/Scripts/app/app.js
@@ -1,33 +1,41 @@
 ﻿"use strict";
 
-// Declare app level module which depends on views, and components
-//angular.module('myApp', [
-//  'ngRoute',
-//  'myApp.view1',
-//  'myApp.view2',
-//  'myApp.version'
-//]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-//    //$locationProvider.hashPrefix('!');
-//    $loationnProvider.html5Mode(true);
+var app = angular.module("myApp", ['ngRoute', 'ui.bootstrap']);
 
-//    $routeProvider.otherwise({ redirectTo: '/view1' });
-//}]);
-
-angular
-    .module("myApp", ['ngRoute', 'ui.bootstrap'])
-    .controller("myController",
-    [
-        "$scope", function($scope) {
-            $scope.message = "Hello World from angular!";
-            $scope.stationIds = ["testStation", "weatherStation1_raspberrypi"];
-            $scope.selectedStation = {
-                stationId: $scope.stationIds[0],
-                latestSensorData: {
-                    temperature: {
-                        value: 25.0,
-                        timestamp : new Date("2001-01-01T12:45:30Z")
+app.config([
+    '$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.when("/index.html/page/:pageName",
+            {
+                template: '<weather-page name="{{pageName}}"></weather-page>',
+                controller: [
+                    '$scope', '$routeParams', function ($scope, $routeParams) {
+                        console.log($routeParams);
+                        $scope.pageName = $routeParams.pageName;
                     }
+                ]
+            })
+            .otherwise({
+                template: '<weather-page name="default"></state-page>'
+            });
+    }
+]);
+
+app.controller("myController",
+[
+    "$scope", function($scope) {
+        $scope.message = "Hello World from angular!";
+        $scope.stationIds = ["testStation", "weatherStation1_raspberrypi"];
+        $scope.selectedStation = {
+            stationId: $scope.stationIds[0],
+            latestSensorData: {
+                temperature: {
+                    value: 25.0,
+                    timestamp: new Date("2001-01-01T12:45:30Z")
                 }
             }
         }
-    ]);
+    }
+]);
+
