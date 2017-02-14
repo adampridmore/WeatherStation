38fc0c3ea2ed4e5e2025f5dc84dee6775b4bd74d
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-14 15:15:21 +0000

    - Added an empty view for allWeatherSensorsView  - Added method to create route controller / templates.

--- WeatherStationPi/WeatherStationServer/app/routing.js
+++ WeatherStationPi/WeatherStationServer/app/routing.js
@@ -1,15 +1,20 @@
 ﻿(function() {
     var configFunction = function($locationProvider, $routeProvider) {
         $locationProvider.html5Mode(true);
-        $routeProvider.when("/V2/MyWeatherStation",
-            {
-                templateUrl: "app/Views/myWeatherStation/myWeatherStationView-template.html",
-                controller: "myWeatherStationView",
-                controllerAs : "vw"
-            })
-            .otherwise({
-                template: "<h1>Otherwise</h1>"
-            });
+
+        var createController = function(name) {
+            return {
+                templateUrl: "app/Views/" + name + "/" + name + "-template.html",
+                controller: name,
+                controllerAs: "vw"
+            }
+        };
+
+        $routeProvider
+            .when("/V2/MyWeatherStation/:stationId", createController("myWeatherStationView"))
+            .when("/V2/MyWeatherStation", createController("myWeatherStationView"))
+            .when("/V2/AllWeatherSensors", createController("allWeatherSensorsView"))
+            .otherwise({ template: "<h1>Otherwise</h1>" });
     }
 
     angular
