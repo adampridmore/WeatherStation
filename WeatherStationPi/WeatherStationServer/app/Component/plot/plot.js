6ea38de2494d2e222aaf84c5160e2f7b4977e4be
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-25 18:53:28 +0000

    - Added a basic component for rendering a google plot  - Added a service for generating unique ids.

---
+++ WeatherStationPi/WeatherStationServer/app/Component/plot/plot.js
@@ -0,0 +1,45 @@
+﻿(function() {
+    var controllerFunction = function(uniqueIdService, $timeout) {
+        var vm = this;
+        vm.elementId = uniqueIdService.getId();
+
+        var renderChart = function() {
+            var data = google.visualization.arrayToDataTable([
+                ['Year', 'Sales', 'Expenses'],
+                ['2004', 1000, 400],
+                ['2005', 1170, 460],
+                ['2006', 660, 1120],
+                ['2007', 1030, 540]
+            ]);
+            var options = {
+                title: vm.title
+            };
+
+            var div = document.getElementById(vm.elementId);
+            console.log(div);
+
+            var chart = new google.visualization.LineChart(div);
+
+            chart.draw(data, options);
+        };
+
+        this.$postLink = () => {
+            // This waits until the page is rendred before attaching the google chart.
+            $timeout(renderChart, 0);
+        };
+
+        this.$onChanges = () => {
+        }
+    };
+
+    angular.module("myApp")
+        .component("plot",
+        {
+            bindings: {
+                title: "@",
+                dataPoints: "<"
+            },
+            controller: ["uniqueIdService", "$timeout", controllerFunction],
+            templateUrl: "app/Component/plot/plot-template.html"
+        });
+})();
---
+++ WeatherStationPi/WeatherStationServer/app/Modules/UniqueIdService.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    var uniqueIdService = function() {
+        var id = 0;
+
+        return {
+            getId: function () {
+                return ++id;
+            }
+        };
+    };
+
+    angular.module("myApp")
+        .factory("uniqueIdService", [uniqueIdService]);
+})();
