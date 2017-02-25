4a26729d4d245bc4761675086e6d36c1e2c8bde4
Adam Pridmore <adam_pridmore@hotmail.com>
2017-02-25 17:11:43 +0000

    - Fixed a bug where the station data points weren't loaded when a stationId wasn't a URL route parameter.

--- WeatherStationPi/WeatherStationServer/app/Views/myWeatherStationView/myWeatherStationView.js
+++ WeatherStationPi/WeatherStationServer/app/Views/myWeatherStationView/myWeatherStationView.js
@@ -2,23 +2,32 @@
     var controller = function($routeParams, $scope, dataService) {
         var currentStationId = $routeParams.stationId;
 
+        var loadStationData = function() {
+            if (currentStationId === undefined) {
+                return;
+            }
+            dataService.getStationData(currentStationId)
+                .then(function(data) {
+                    // {"sensorTimestampUtc":"2016-11-17T17:42:29.477","value":27.623125076293945,"sensorType":"Temperature"}
+                    $scope.lastStationData = data.lastValues;
+                });
+        };
+        
         $scope.currentStationId = currentStationId;
         $scope.stationIds = [];
         $scope.lastStationData = [];
 
         dataService.getStationIds()
             .then(function(stationIds) {
-                //console.log(stationIds);
                 $scope.stationIds = stationIds;
+
+                if (currentStationId === undefined && stationIds.length > 0) {
+                    currentStationId = stationIds[0];
+                    loadStationData();
+                }
             });
 
-        if (currentStationId !== undefined) {
-            dataService.getStationData(currentStationId)
-                .then(function(data) {
-                    // {"sensorTimestampUtc":"2016-11-17T17:42:29.477","value":27.623125076293945,"sensorType":"Temperature"}
-                    $scope.lastStationData = data.lastValues;
-                });
-        }
+        loadStationData();
     };
 
     angular
