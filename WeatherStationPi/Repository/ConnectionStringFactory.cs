2ce150c1d2c474745f86104d6de639993c089120
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-11 17:38:43 +0000

    - Added a FSharp script to load all the data points.

---
+++ WeatherStationPi/Repository/ConnectionStringFactory.cs
@@ -0,0 +1,25 @@
+﻿using Repository.Interfaces;
+
+namespace Repository
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class ConnectionStringFactory : IConnectionStringFactory
+    {
+        private readonly string _connectionString;
+
+        private ConnectionStringFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static IConnectionStringFactory CreateFromConnectionString(string connectionString  )
+        {
+            return new ConnectionStringFactory(connectionString);
+        }
+
+        public string GetNameOrConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
---
+++ WeatherStationPi/WeatherAnalytics/WeatherForecast.fsx
@@ -0,0 +1,22 @@
+﻿#r @"..\Repository\bin\Debug\Repository.dll"
+
+let connectionString = "server=.\SQLEXPRESS;database=WeatherStation;Integrated Security = True; Connect Timeout = 5";
+let factory = Repository.ConnectionStringFactory.CreateFromConnectionString(connectionString)
+
+let repository = new Repository.DataPointRepository(factory)
+
+let data = repository.FindAllByStationId("weatherStation1_raspberrypi")
+
+let startDate = new System.DateTime(2016,11,18,0,0,0, System.DateTimeKind.Utc)
+let endDate = new System.DateTime(2016,11,19,0,0,0, System.DateTimeKind.Utc)
+
+
+let dataToString (dataPoint: Repository.RepositoryDto.DataPoint) =
+    sprintf "%s (%s) %f" (dataPoint.SensorTimestampUtc.ToString("o")) dataPoint.SensorType dataPoint.SensorValueNumber
+
+data 
+|> Seq.filter (fun dp -> dp.SensorTimestampUtc < endDate)
+|> Seq.filter (fun dp -> dp.SensorTimestampUtc > startDate)
+|> Seq.filter (fun dp -> dp.SensorType = "Temperature")
+|> Seq.sortBy (fun dp -> dp.SensorTimestampUtc)
+|> Seq.iter(fun dp-> printfn "%s" (dp |> dataToString ) )
