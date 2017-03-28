b9df9316b2ea3d751e5a40764e71cf24a7bfe21f
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-28 20:11:50 +0100

    - Initial Selenium test.

---
+++ WeatherStationPi/WeatherStationServer.ApiTests/WebSiteTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WeatherStationServer.ApiTests
+{
+    [TestFixture]
+    [Category("WebSiteIntegration")]
+    public class WebSiteTest
+    {
+        [Test]
+        public void Go()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+
+            webDriver
+                .Navigate()
+                .GoToUrl("http://localhost:59653/V2/");
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            IWebElement dropDown = webDriver.FindElement(By.Id("single-button"));
+            Assert.That(dropDown.Text, Is.EqualTo("apiTestsStationId"));
+
+            //var testStationOption = dropDown.FindElement(By.LinkText("testStation"));
+            //testStationOption.Click();
+
+            //webDriver.Close();
+        }
+    }
+}
