ab0fea0ba32f2a8511cc14e5e8f7f4242525fffa
Adam Pridmore <adam_pridmore@hotmail.com>
2017-03-29 21:51:17 +0100

    - Implemented some MongoDB repository tests.

--- WeatherStationPi/Repository.UnitTests/DataPointRepositoryMongoDbTests.cs
+++ WeatherStationPi/Repository.UnitTests/DataPointRepositoryMongoDbTests.cs
@@ -4,15 +4,22 @@ using SimpleInjector;
 
 namespace Repository.UnitTests
 {
-	// Commented out as xunit doesn't let you ignore this fixture.    //public class DataPointRepositoryMongoDbTests : DataPointRepositoryTests
+    // Commented out as xunit doesn't let you ignore this fixture.
+    //public class DataPointRepositoryMongoDbTests : DataPointRepositoryTests
     //{
     //    protected override Container CreateContainer()
     //    {
     //            var container = new Container();
 
+                container.Register<IDataPointRepository, DataPointMongoDbRepository>();
+                container.Register<IConnectionStringFactory, UnitTestMongoDbConnectionStringFactory>();
     //            container.Register<IDataPointRepository, DataPointMongoDbRepository>();
     //            container.Register<IConnectionStringFactory, UnitTestMongoDbConnectionStringFactory>();
 
+                container.Verify();
+                return container;
+        }
+    }
     //            container.Verify();
     //            return container;
     //    }
