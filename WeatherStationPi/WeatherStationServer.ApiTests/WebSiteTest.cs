3610ca8afcbb728bc3f12dad85d4e52b6b9bddb4
AdamPridmore <adam.pridmore@hotmail.com>
2017-04-24 14:21:47 +0100

    - Changed selenium test to use a Page POJO for accessing page elemenets.

--- WeatherStationPi/WeatherStationServer.ApiTests/WebSiteTest.cs
+++ WeatherStationPi/WeatherStationServer.ApiTests/WebSiteTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace WeatherStationServer.ApiTests
 {
@@ -9,24 +10,54 @@ namespace WeatherStationServer.ApiTests
     [Category("WebSiteIntegration")]
     public class WebSiteTest
     {
+        //private string _url = "http://localhost:59653/V2/";
+        private string _url = "http://weatherstat.azurewebsites.net/v2";
+
         [Test]
         public void Go()
         {
-            IWebDriver webDriver = new ChromeDriver();
+            var myWeatherStationPage = GoToMyWeatherStationPage();
 
-            webDriver
-                .Navigate()
-                .GoToUrl("http://localhost:59653/V2/");
-
-            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
-
-            IWebElement dropDown = webDriver.FindElement(By.Id("single-button"));
-            Assert.That(dropDown.Text, Is.EqualTo("apiTestsStationId"));
+            Assert.That(myWeatherStationPage.SelectedDropDownText, Is.EqualTo("weatherStation1_Calf"));
 
             //var testStationOption = dropDown.FindElement(By.LinkText("testStation"));
             //testStationOption.Click();
 
             //webDriver.Close();
+        }
+
+        private MyWeatherStationPage GoToMyWeatherStationPage()
+        {
+            IWebDriver webDriver = new ChromeDriver();
+
+            webDriver
+                .Navigate()
+                .GoToUrl(_url)
+                ;
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+            var page = SeleniumPageFactory<MyWeatherStationPage>(webDriver);
+            return page;
+        }
+
+        public T SeleniumPageFactory<T>(IWebDriver webDriver) where T : new()
+        {
+            var page = new T();
+
+            PageFactory.InitElements(page, new RetryingElementLocator(webDriver, TimeSpan.FromSeconds(1)));
+
+            return page;
+        }
+
+        public class MyWeatherStationPage
+        {
+            [FindsBy(How = How.Id, Using = "single-button")]
+            private IWebElement DropDown { get; set; }
+
+            public string SelectedDropDownText
+            {
+                get { return DropDown.Text; }
+            }
         }
     }
 }
